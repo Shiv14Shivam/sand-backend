315f4ffb3ca855f197989dc891d2e90936619fa7
Shiv14Shivam <ramsingh69929@gmail.com>
2026-01-16 01:52:52 +0530

    Update routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 /*
 |--------------------------------------------------------------------------
